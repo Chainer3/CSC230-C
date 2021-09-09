fa582b67765ae828e6b52955bcb6a63865fbbcd8
Stephen Gonsalves <dkgonsal@engr-ras-207.eos.ncsu.edu>
2021-09-09 10:16:20 -0400

    Initial push of base files

---
+++ p2/base.c
@@ -0,0 +1,15 @@
+#include "base.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "operation.h"
+
+// This preprocessor syntax makes it so we can override the value of BASE with
+// a compiler option.  For some desired base, n, we can compile with: -DBASE=n
+// Don't change these three lines of preprocessor directives.
+#ifndef BASE
+/** Base this program uses for input and output. */
+#define BASE 7
+#endif
